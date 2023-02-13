33e018e7f23cd90a0774d005cad8afe68e1bd5f8
Jason Young <jason_y54@protonmail.com>
2023-02-13 10:38:41 -0800

    Remove scala code

--- kafka_docker/start.py
+++ kafka_docker/start.py
@@ -5,10 +5,10 @@ path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 
 os.system("docker compose up -d")
-time.sleep(5)
+time.sleep(10)
 
 os.system("docker compose exec broker \
-    kafka-topics --create --topic RawLogsTopic \
+    kafka-topics --create --topic raw-logs \
     --bootstrap-server localhost:9092 \
     --replication-factor 1 --partitions 1")
 
